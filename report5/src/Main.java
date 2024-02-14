fb46bcc5f8f2f6792168dd371b0b18238871f28d
Naka Kouto <e205750@ie.u-ryukyu.ac.jp>
2024-02-14 23:03:13 +0900

    second

--- report5/src/Main.java
+++ report5/src/Main.java
@@ -1,8 +1,11 @@
-
-
 public class Main {
-    public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
-    }    
+    public static void main(String[] args){
+    String str = null;
+    try {
+        str.length(); //処理したいコード
+    } catch (NullPointerException e) {
+        System.out.println("NullPointerExceptionが発生しました");//Exceptionが起きたときの処理
+        System.out.println("Exception message: " + e.getMessage());
+    } 
+    }
 }
